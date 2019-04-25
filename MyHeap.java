0d94df3abe17da4d68d2dec82c8b1cbdab50cc19
Christy Guan <cguan00@stuy.edu>
2019-04-25 11:29:40 -0400

    wrote pushDown, need to write the helper swap method

--- MyHeap.java
+++ MyHeap.java
@@ -1,10 +1,40 @@
+import java.util.*;
+
 public class MyHeap{
 
   // - size  is the number of elements in the data array.
-  // - push the element at index i downward into the correct position. This will swap with the larger of the child nodes provided thatchild is larger. This stops when a leaf is reached, or neither child is larger. [ should be O(logn) ]
+  // - push the element at index i downward into the correct position. This will swap with the larger of the child nodes provided that child is larger. This stops when a leaf is reached, or neither child is larger. [ should be O(logn) ]
   // - precondition: index is between 0 and size-1 inclusive
   // - precondition: size is between 0 and data.length-1 inclusive.
   private static void pushDown(int[] data, int size, int index){
+    boolean sorted = false;
+    while(!sorted){
+      int leftIndex = index * 2 + 1;//index of left child
+      int rightIndex = index * 2 + 2;//index of right child
+
+      if(leftIndex >= size && rightIndex >= size){//if no child, index will be greater than size
+        sorted = true;//this will exit the while loop
+      } else{//if there are children
+        int max = data[index];//set the max to the parent
+
+        if(leftIndex < size){//if there is a left child
+          max = Math.max(max, data[leftIndex]); //finds max between max and left child
+        }
+
+        if(rightIndex < size){//if there is a right child
+          max = Math.max(max, data[rightIndex]); //finds max between max and right child
+        }
+
+        if(max == data[index]){//if the maximum is the parent, then it's already sorted
+          sorted = true;//this will exit the while loop
+        } else if(leftIndex < size && max == data[leftIndex]){
+          swap(data, index, leftIndex);
+        } else if(rightIndex < size && max == data[rightIndex]){
+          swap(data, index, rightIndex);
+        }
+
+      }
+    }
 
   }
 
